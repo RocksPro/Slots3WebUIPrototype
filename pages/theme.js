968b5913c14da49d27f5a061dff09a410ffad476
Oleg <dev-mac1@rocksplay.com>
2016-06-30 10:05:34 +0300

    Добавлен скрипт обработки параметров имен тем в URL

---
+++ pages/theme.js
@@ -0,0 +1,30 @@
+function themeLoaded()
+{
+	var opened_popup = $_GET('t');
+	
+	if (opened_popup == "farm")
+	{
+		
+	}
+	else if (opened_popup == "rome")
+	{
+		
+	}
+	else if (opened_popup == "aztec")
+	{
+		
+	}
+	else if (opened_popup == "classic")
+	{
+		
+	}
+	else if (opened_popup == "billionaire")
+	{
+
+	}
+	else if (opened_popup == "africa")
+	{
+		
+	}
+}
+
