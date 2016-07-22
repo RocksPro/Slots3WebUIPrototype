dc87e1bc8b8ea041744b2627fd71789ebbaee7de
Oleg <oleg@rocksplay.com>
2016-07-22 09:52:11 +0300

    Центровка надписей на кнопках Bottom Panel

--- lobbyBottomPanelConstructor.js
+++ lobbyBottomPanelConstructor.js
@@ -45,8 +45,8 @@ function ConstructBittomPanel()
 	const uiGameInboxButtonHeightScale = 0.15;
 	const uiGameInboxButtonLeftScale = 0;
 	const uiGameInboxButtonTopScale = 0.85;
-	const uiGameInboxButtonLabelLocalLeftScale = 0.5;
-	const uiGameInboxButtonLabelLocalTopScale = 0.5;
+	const uiGameInboxButtonLabelLocalLeftScale = 0.3;
+	const uiGameInboxButtonLabelLocalTopScale = 0.4;
 	uiGameInboxButtonWidth = uiGameWidth * uiGameInboxButtonWidthScale;
 	uiGameInboxButtonHeight = uiGameHeight * uiGameInboxButtonHeightScale;
 	uiGameInboxButtonLeft = uiGameWidth * uiGameInboxButtonLeftScale;
@@ -59,7 +59,7 @@ function ConstructBittomPanel()
 	const uiGameGiftsButtonLeftScale = 0.145;
 	const uiGameGiftsButtonTopScale = 0.8705;
 	const uiGameGiftsButtonLabelLocalLeftScale = 0.5;
-	const uiGameGiftsButtonLabelLocalTopScale = 0.5;
+	const uiGameGiftsButtonLabelLocalTopScale = 0.4;
 	uiGameGiftsButtonWidth = uiGameWidth * uiGameGiftsButtonWidthScale;
 	uiGameGiftsButtonHeight = uiGameHeight * uiGameGiftsButtonHeightScale;
 	uiGameGiftsButtonLeft = uiGameWidth * uiGameGiftsButtonLeftScale;
@@ -71,8 +71,8 @@ function ConstructBittomPanel()
 	const uiGameBuyButtonHeightScale = 0.1285;
 	const uiGameBuyButtonLeftScale = 0.655;
 	const uiGameBuyButtonTopScale = 0.8705;
-	const uiGameBuyButtonLabelLocalLeftScale = 0.5;
-	const uiGameBuyButtonLabelLocalTopScale = 0.5;
+	const uiGameBuyButtonLabelLocalLeftScale = 0.3;
+	const uiGameBuyButtonLabelLocalTopScale = 0.4;
 	uiGameBuyButtonWidth = uiGameWidth * uiGameBuyButtonWidthScale;
 	uiGameBuyButtonHeight = uiGameHeight * uiGameBuyButtonHeightScale;
 	uiGameBuyButtonLeft = uiGameWidth * uiGameBuyButtonLeftScale;
@@ -84,8 +84,8 @@ function ConstructBittomPanel()
 	const uiGameFriendsButtonHeightScale = 0.15;
 	const uiGameFriendsButtonLeftScale = 0.8;
 	const uiGameFriendsButtonTopScale = 0.85;
-	const uiGameFriendsButtonLabelLocalLeftScale = 0.5;
-	const uiGameFriendsButtonLabelLocalTopScale = 0.5;
+	const uiGameFriendsButtonLabelLocalLeftScale = 0.4;
+	const uiGameFriendsButtonLabelLocalTopScale = 0.4;
 	uiGameFriendsButtonWidth = uiGameWidth * uiGameFriendsButtonWidthScale;
 	uiGameFriendsButtonHeight = uiGameHeight * uiGameFriendsButtonHeightScale;
 	uiGameFriendsButtonLeft = uiGameWidth * uiGameFriendsButtonLeftScale;
@@ -119,22 +119,22 @@ function ConstructBittomPanel()
 	var bottomPanelHTML = '<div id = "bottomPanel">\
 								<button id = "inboxButton"; onclick = \"showInboxPopup()\" style = \"font-size:'+uiGameFontSize+'px; color: #ffffff; border:none; background-color:Transparent; cursor:pointer; position: absolute; width: ' + uiGameInboxButtonWidth + 'px; height: ' + uiGameInboxButtonHeight + 'px; left: ' + uiGameInboxButtonLeft + 'px; top: ' + uiGameInboxButtonTop + 'px \">\
 									<img id = "inboxButtonTexture"; src = "imgs/button_inbox.png" style="position:absolute; width:' +  uiGameInboxButtonWidth + 'px; height: ' + uiGameInboxButtonHeight + 'px; left: 0px; top: 0px ">\
-										<div style="font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameInboxButtonLabelLocalLeft+'px; top:'+uiGameInboxButtonLabelLocalTop+'px">Inbox '+giftscount+' </div>\
+										<div style="font-family:AeroMaticsBold; font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameInboxButtonLabelLocalLeft+'px; top:'+uiGameInboxButtonLabelLocalTop+'px">Inbox '+giftscount+' </div>\
 									</img>\ \
 								</button>\
 								<button id = "giftsButton"; onclick = \"showInboxPopup()\" style = \"font-size:'+uiGameFontSize+'px; color: #ffffff; border:none; background-color:Transparent; cursor:pointer; position: absolute; width: ' + uiGameGiftsButtonWidth + 'px; height: ' + uiGameGiftsButtonHeight + 'px; left: ' + uiGameGiftsButtonLeft + 'px; top: ' + uiGameGiftsButtonTop + 'px \">\
 									<img id = "GiftsButtonTexture"; src = "imgs/button_gifts.png" style="position:absolute; width:' +  uiGameGiftsButtonWidth + 'px; height: ' + uiGameGiftsButtonHeight + 'px; left: 0px; top: 0px ">\
-										<div style="font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameGiftsButtonLabelLocalLeft+'px; top:'+uiGameGiftsButtonLabelLocalTop+'px">Gifts '+giftscount+' </div>\
+										<div style="font-family:AeroMaticsBold; font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameGiftsButtonLabelLocalLeft+'px; top:'+uiGameGiftsButtonLabelLocalTop+'px">Gifts '+giftscount+' </div>\
 									</img>\ \
 								</button>\
 								<button id = "buyButton"; onclick = \"showInboxPopup()\" style = \"font-size:'+uiGameFontSize+'px; color: #ffffff; border:none; background-color:Transparent; cursor:pointer; position: absolute; width: ' + uiGameBuyButtonWidth + 'px; height: ' + uiGameBuyButtonHeight + 'px; left: ' + uiGameBuyButtonLeft + 'px; top: ' + uiGameBuyButtonTop + 'px \">\
 									<img id = "BuyButtonTexture"; src = "imgs/button_buy.png" style="position:absolute; width:' +  uiGameBuyButtonWidth + 'px; height: ' + uiGameBuyButtonHeight + 'px; left: 0px; top: 0px ">\
-										<div style="font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameBuyButtonLabelLocalLeft+'px; top:'+uiGameBuyButtonLabelLocalTop+'px">Buy</div>\
+										<div style="font-family:AeroMaticsBold; font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameBuyButtonLabelLocalLeft+'px; top:'+uiGameBuyButtonLabelLocalTop+'px">Buy</div>\
 									</img>\ \
 								</button>\
 								<button id = "friendsButton"; onclick = \"showPopup()\" style = \"font-size:'+uiGameFontSize+'px; color: #ffffff; border:none; background-color:Transparent; cursor:pointer;  position: absolute; width: ' + uiGameFriendsButtonWidth + 'px; height: ' + uiGameFriendsButtonHeight + 'px; left: ' + uiGameFriendsButtonLeft + 'px; top: ' + uiGameFriendsButtonTop + 'px \"> \
 									<img id = "friendsButtonTexture"; src = "imgs/button_friends.png" style="position:absolute; width:' +  uiGameFriendsButtonWidth + 'px; height: ' + uiGameFriendsButtonHeight + 'px; left: 0px; top: 0px ">\
-										<div style="font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameFriendsButtonLabelLocalLeft+'px; top:'+uiGameFriendsButtonLabelLocalTop+'px">Friends</div>\
+										<div style="font-family:AeroMaticsBold; font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameFriendsButtonLabelLocalLeft+'px; top:'+uiGameFriendsButtonLabelLocalTop+'px">Friends</div>\
 									</img>\ \
 								</button>\
 								<button id = "bonusCollectButton"; onclick = \"collectBonus()\" style = \"background-color: Transparent; border: none; cursor:pointer;  position: absolute; position: absolute; width: ' + uiGameMegaBonusPanelWidth + 'px; height: ' + uiGameMegaBonusPanelHeight + 'px; left: ' + uiGameMegaBonusPanelLeft + 'px; top: ' + uiGameMegaBonusPanelTop + 'px">\
