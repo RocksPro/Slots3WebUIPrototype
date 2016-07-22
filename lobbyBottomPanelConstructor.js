d64db450e9edaa1df1f8af80a07f80088adbfe23
Oleg <oleg@rocksplay.com>
2016-07-22 09:01:05 +0300

    Добавлена кнопка Gifts

--- lobbyBottomPanelConstructor.js
+++ lobbyBottomPanelConstructor.js
@@ -12,6 +12,13 @@ var uiGameFriendsButtonTop;
 var uiGameFriendsButtonLabelLocalLeft;
 var uiGameFriendsButtonLabelLocalTop;
 
+var uiGameGiftsButtonWidth;
+var uiGameGiftsButtonHeight;
+var uiGameGiftsButtonLeft;
+var uiGameGiftsButtonTop;
+var uiGameGiftsButtonLabelLocalLeft;
+var uiGameGiftsButtonLabelLocalTop;
+
 var uiGameMegaBonusPanelWidth;
 var uiGameMegaBonusPanelHeight;
 var uiGameMegaBonusPanelLeft;
@@ -21,6 +28,9 @@ var uiGameMegaBonusIndicateWidth;
 var uiGameMegaBonusIndicateHeight;
 var uiGameMegaBonusIndicateLeft;
 var uiGameMegaBonusIndicateTop;
+
+var uiGameMegaBonusLabelLeft;
+var uiGameMegaBonusLebelTop;
 
 function ConstructBittomPanel()
 {
@@ -36,6 +46,19 @@ function ConstructBittomPanel()
 	uiGameInboxButtonTop = uiGameHeight * uiGameInboxButtonTopScale;
 	uiGameInboxButtonLabelLocalLeft = uiGameInboxButtonWidth * uiGameInboxButtonLabelLocalLeftScale;
 	uiGameInboxButtonLabelLocalTop = uiGameInboxButtonHeight * uiGameInboxButtonLabelLocalTopScale;
+	
+	const uiGameGiftsButtonWidthScale = 0.2;
+	const uiGameGiftsButtonHeightScale = 0.1285;
+	const uiGameGiftsButtonLeftScale = 0.145;
+	const uiGameGiftsButtonTopScale = 0.8705;
+	const uiGameGiftsButtonLabelLocalLeftScale = 0.5;
+	const uiGameGiftsButtonLabelLocalTopScale = 0.5;
+	uiGameGiftsButtonWidth = uiGameWidth * uiGameGiftsButtonWidthScale;
+	uiGameGiftsButtonHeight = uiGameHeight * uiGameGiftsButtonHeightScale;
+	uiGameGiftsButtonLeft = uiGameWidth * uiGameGiftsButtonLeftScale;
+	uiGameGiftsButtonTop = uiGameHeight * uiGameGiftsButtonTopScale;
+	uiGameGiftsButtonLabelLocalLeft = uiGameGiftsButtonWidth * uiGameGiftsButtonLabelLocalLeftScale;
+	uiGameGiftsButtonLabelLocalTop = uiGameGiftsButtonHeight * uiGameGiftsButtonLabelLocalTopScale;
 
 	const uiGameFriendsButtonWidthScale = 0.2;
 	const uiGameFriendsButtonHeightScale = 0.15;
@@ -68,10 +91,20 @@ function ConstructBittomPanel()
 	uiGameMegaBonusIndicateLeft = uiGameWidth * uiGameMegaBonusIndicateLeftScale;
 	uiGameMegaBonusIndicateTop = uiGameHeight * uiGameMegaBonusIndicateTopScale;
 	
+	const uiGameMegaBonusLabelLeftScale = 0.435;
+	const uiGameMegaBonusLabelTopScale = 0.92;
+	uiGameMegaBonusLabelLeft = uiGameWidth * uiGameMegaBonusLabelLeftScale;
+	uiGameMegaBonusLabelTop = uiGameHeight * uiGameMegaBonusLabelTopScale;
+	
 	var bottomPanelHTML = '<div id = "bottomPanel">\
 								<button id = "inboxButton"; onclick = \"showInboxPopup()\" style = \"font-size:'+uiGameFontSize+'px; color: #ffffff; border:none; background-color:Transparent; cursor:pointer; position: absolute; width: ' + uiGameInboxButtonWidth + 'px; height: ' + uiGameInboxButtonHeight + 'px; left: ' + uiGameInboxButtonLeft + 'px; top: ' + uiGameInboxButtonTop + 'px \">\
 									<img id = "inboxButtonTexture"; src = "imgs/button_inbox.png" style="position:absolute; width:' +  uiGameInboxButtonWidth + 'px; height: ' + uiGameInboxButtonHeight + 'px; left: 0px; top: 0px ">\
 										<div style="font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameInboxButtonLabelLocalLeft+'px; top:'+uiGameInboxButtonLabelLocalTop+'px">Inbox '+giftscount+' </div>\
+									</img>\ \
+								</button>\
+								<button id = "giftsButton"; onclick = \"showInboxPopup()\" style = \"font-size:'+uiGameFontSize+'px; color: #ffffff; border:none; background-color:Transparent; cursor:pointer; position: absolute; width: ' + uiGameGiftsButtonWidth + 'px; height: ' + uiGameGiftsButtonHeight + 'px; left: ' + uiGameGiftsButtonLeft + 'px; top: ' + uiGameGiftsButtonTop + 'px \">\
+									<img id = "GiftsButtonTexture"; src = "imgs/button_gifts.png" style="position:absolute; width:' +  uiGameGiftsButtonWidth + 'px; height: ' + uiGameGiftsButtonHeight + 'px; left: 0px; top: 0px ">\
+										<div style="font-size:'+uiGameFontSize+'px; position:absolute; left:'+uiGameGiftsButtonLabelLocalLeft+'px; top:'+uiGameGiftsButtonLabelLocalTop+'px">Gifts '+giftscount+' </div>\
 									</img>\ \
 								</button>\
 								<button id = "friendsButton"; onclick = \"showPopup()\" style = \"font-size:'+uiGameFontSize+'px; color: #ffffff; border:none; background-color:Transparent; cursor:pointer;  position: absolute; width: ' + uiGameFriendsButtonWidth + 'px; height: ' + uiGameFriendsButtonHeight + 'px; left: ' + uiGameFriendsButtonLeft + 'px; top: ' + uiGameFriendsButtonTop + 'px \"> \
@@ -81,9 +114,9 @@ function ConstructBittomPanel()
 								</button>\
 								<button id = "bonusCollectButton"; onclick = \"collectBonus()\" style = \"background-color: Transparent; border: none; cursor:pointer;  position: absolute; position: absolute; width: ' + uiGameMegaBonusPanelWidth + 'px; height: ' + uiGameMegaBonusPanelHeight + 'px; left: ' + uiGameMegaBonusPanelLeft + 'px; top: ' + uiGameMegaBonusPanelTop + 'px">\
 									<img id = "bonusCollectButtonTexture"; src = "imgs/bonus_plate.png" style="position:absolute; width: ' + uiGameMegaBonusPanelWidth + 'px; height: ' + uiGameMegaBonusPanelHeight*1.2 + 'px; left: 0px; top: 0px"></img>\
-									<div id = "bonusCollectInfoLabel"; style = \"font-size:'+uiGameFontSize*0.8+'px; text-align: center; color: #ffffff; background-color: Trnsparent; position: absolute; width: ' + uiGameMegaBonusPanelWidth + 'px; height: ' + uiGameMegaBonusPanelHeight + 'px; left: 0px; top: '+uiGameMegaBonusPanelHeight*0.12+'px \">'+ megaBonusInfoText + ' ' + bonusButtonText + '</div>\
 								</button>\
 								<img id = "bonusIndicatorTexture"; src = "imgs/indicate.png" style="position:absolute; width: ' + uiGameMegaBonusIndicateWidth + 'px; height: ' + uiGameMegaBonusIndicateHeight + 'px; left: '+uiGameMegaBonusIndicateLeft+'px; top: '+uiGameMegaBonusIndicateTop+'px"></img>\
+								<div id = "bonusCollectInfoLabel"; style = \"font-size:'+uiGameFontSize+'px; text-align: center; color: #ffffff; background-color: Trnsparent; position: absolute; left: ' + uiGameMegaBonusLabelLeft + 'px; top: ' + uiGameMegaBonusLabelTop + 'px;\">'+ megaBonusInfoText + ' ' + bonusButtonText + '</div>\
 							</div>';
 	return bottomPanelHTML;
 }
